8d0080e8c02abeccb50540d0ccb52d44ab50ff2b
vagrant <vagrant@lighthouse-labs>
2014-01-28 22:38:25 +0000

    event model created

---
+++ db/migrate/20140128223601_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :artist
+      t.string :venue
+      t.string :location
+      t.datetime :concert_date
+      t.text :description
+      t.string :artist_image_url
+
+      t.timestamps
+    end
+  end
+end
