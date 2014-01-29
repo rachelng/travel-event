6b8a5de3eaaf86cefda6f1b92aede536de40fb4e
vagrant <vagrant@lighthouse-labs>
2014-01-29 21:58:05 +0000

    search takes travel dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
 
 def self.search(options)
-	am = options[:concert_date].to_date.beginning_of_day
-	pm = options[:concert_date].to_date.end_of_day
+	start_day = options[:start_date].to_date.beginning_of_day
+	final_day = options[:end_date].to_date.end_of_day
 
   if options
-    find(:all, :conditions =>["location = ? AND concert_date >= ? AND concert_date <= ?", options[:location], am, pm]) 
+    find(:all, :conditions =>["location = ? AND concert_date >= ? AND concert_date <= ?", options[:location], start_day, final_day]) 
     #change to where
   else
     find(:all)
